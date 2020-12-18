d28d24af3dfcb72cdcae1ac7fdc202b83a3a8e78
Bulavkin Nikolay <bulnikn@gmail.com>
2020-12-18 21:14:17 +0200

    home pc

--- myDictCSV.py
+++ myDictCSV.py
@@ -1,27 +1,36 @@
 import myDict
 import csv
+import os
 
 
-class MyDictCSV(object):#myDict.MyDict
+class MyDictCSV(myDict.MyDict):#
 
     def __init__(self):
-        # self.file_csv = self.get_random_name() + ".csv"
-        self.file_csv = "11111111111111.csv"
-
-    def write_csv(self, key, value=''):
-        with open(self.file_csv, 'a', newline='', encoding="utf-8") as csvfile:
-            writer = csv.writer(csvfile, delimiter=' ')
-            writer.writerow(key+";"+ value)
+        self.file_csv = self.get_random_name() + ".csv"
 
 
-    def read_csv(self):
+    def _write_csv(self, key, value=''):
+        with open(self.file_csv, 'w', newline='',  encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile, delimiter=';')
+            writer.writerow((key, value))
+
+
+    def _read_csv(self):
         with open(self.file_csv, 'r', newline='', encoding="utf-8") as csvfile:  # newline /n
-            reader = csv.reader(csvfile, delimiter=' ')
+            reader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONE)
             return [row for row in reader]  # watch in reader
+
+    def __del__(self):
+        os.remove(self.file_csv)
+
+    def __len__(self):
+        return len(self._read_csv())
+
 
 
 
 c= MyDictCSV()
-c.write_csv("12","Andrey")
-c.write_csv("13","Dmitriy;")
-print(c.read_csv())
+c._write_csv("12","Andrey")
+c._write_csv("13","Dmitriy")
+print(c._read_csv())
+print(len(c))
