cebde84b866466636bab5a4d1c178cc3fdae3436
CAT <bulnikn@gmail.com>
2021-01-20 16:12:58 +0200

    work netbook

--- ste/tools.py
+++ ste/tools.py
@@ -1,12 +1,18 @@
 import time
 
+def format_output(func):
+    def inner(*arg, **kwargs):
+        result = func(*arg, **kwargs)
+        return f"Exception {arg[1]} was raises {arg[0]} at {result} ns ({result/1e9}s) 1={result/arg[0]} ns"
+
+    return inner
 
 def timer(func):
     def inner(*arg, **kwargs):
         start_timer = time.perf_counter_ns()
         func(*arg, **kwargs)
         stop_timer = time.perf_counter_ns()
-        return stop_timer - start_timer
+        return stop_timer - start_timer  # ns
 
     return inner
 
@@ -14,6 +20,10 @@ def timer(func):
 def get_list(list_exc, output):
     return list_exc, output
 
+
+
+
+@format_output
 @timer
 def make_count(count, name_exc):
     count_copy = count
@@ -23,6 +33,4 @@ def make_count(count, name_exc):
         except name_exc:
             pass
         count_copy -= 1
-
-
 
