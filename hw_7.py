af190fa7723e06521ea8013df3a2ce05ae1617f2
Bulavkin Nikolay <bulnikn@gmail.com>
2021-01-23 21:26:27 +0200

    Merge branch 'master' of https://github.com/BulavkinNN/homeWork
    # Conflicts:
    #	.idea/homeWork.iml
    #	.idea/misc.xml

--- hw_7.py
+++ hw_7.py
@@ -22,4 +22,38 @@ def se():
     except SyntaxError:
         pass
 
+
 # se()
+#////////////////////////////////////////////////// traceback
+
+import sys
+import traceback
+
+
+def recur_err(n):
+    global count_recursion
+    if n == 1:
+        return "error"
+    count_recursion += 1
+    print("Recur №", count_recursion)
+    return n + recur_err(n - 1)
+
+
+count_recursion = 0
+# Set max recursion (1000 default)
+sys.setrecursionlimit(50002)
+
+#print(sys.tracebacklimit(2000))
+
+try:
+    print(recur_err(40000))
+except Exception as e:
+    traceback.print_exc()
+    
+    print("Exception ==",e)
+    print("limit recurs= ", sys.getrecursionlimit())
+    print("Last recur №", count_recursion)
+
+finally:
+    traceback_obj = sys.exc_info()[2]
+    traceback.print_tb(traceback_obj)
