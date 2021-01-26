0f8dfb4257aa28e3e7aec81337a491217435c399
Bulavkin Nikolay <bulnikn@gmail.com>
2021-01-26 21:27:36 +0200

    Merge branch 'master' of https://github.com/BulavkinNN/homeWork
    # Conflicts:
    #	.idea/homeWork.iml
    #	.idea/misc.xml

--- hw_8.py
+++ hw_8.py
@@ -1,5 +1,7 @@
 import time
-import exifread
+
+
+# import exifread
 
 
 def timer(func):
@@ -17,9 +19,6 @@ def read_from_file(file):
     file.read()
 
 
-progress = 1
-check_time = 0
-check_time_prev = 0
 buffer_list = [0, 2]
 result_table = dict()
 
@@ -33,7 +32,6 @@ while True:
         break
     result_table[buffer] = check_time
     print(f"buf= {buffer}, check_time= {check_time:,}")
-    check_time_prev = check_time
     buffer_list.append(buffer_list[-1] * 2)  # Make list [0,2,4,8,16,32 and ....
 
 # print result
@@ -45,12 +43,11 @@ for k, v in result_table.items():
 
 # draw result
 v_max = max(result_table.values())
-scale = (50/v_max)
+scale = (50 / v_max)
 sort_key = sorted(result_table.keys())
-print(f"\nBuffering      {(' '*50):<50} Time, ns\n")
+print(f"\nBuffering      {(' ' * 50):<50} Time, ns\n")
 for key in sort_key:
-    print(f"{key:<13,} {('*'*int(result_table[key]*scale)):<50} {result_table[key]:,}")
-
+    print(f"{key:<13,} {('*' * int(result_table[key] * scale)):<50} {result_table[key]:,}")
 
 # /////////////////////////////////////////
 print("Read EXIF data from jpq file")
@@ -66,6 +63,7 @@ marker_mapping = {
     b'\xff\xd9': "End of Image"
 }
 
+
 def ord_(dta):
     if isinstance(dta, str):
         return ord(dta)
@@ -76,26 +74,25 @@ def _find_jpeg_exif(file):
     file.seek(2)
     marker = file.read(2)
 
-
     if marker == b'\xff\xe0':
         print("Default header")
-        lenth_data = int.from_bytes(file.read(2),'big')
+        lenth_data = int.from_bytes(file.read(2), 'big')
 
-        if lenth_data==16:
+        if lenth_data == 16:
             print("All norm!")
-        print(f"Identifier={file.read(4).decode()}{int.from_bytes(file.read(1),'big')}")
-        print(f"version= {int.from_bytes(file.read(1),'big')}.{int.from_bytes(file.read(1),'big')}")
+        print(f"Identifier={file.read(4).decode()}{int.from_bytes(file.read(1), 'big')}")
+        print(f"version= {int.from_bytes(file.read(1), 'big')}.{int.from_bytes(file.read(1), 'big')}")
         print(f"units= {int.from_bytes(file.read(1), 'big')}")
-        print(f"density= {int.from_bytes(file.read(2),'big')}x{int.from_bytes(file.read(2),'big')}")
+        print(f"density= {int.from_bytes(file.read(2), 'big')}x{int.from_bytes(file.read(2), 'big')}")
         print(f"trumbnail= {int.from_bytes(file.read(1), 'big')}x{int.from_bytes(file.read(1), 'big')}")
+
 
 with open(file_name, "rb") as file:
     data = file.read(2)
-    print (data)
+    print(data)
     if data[0:2] == b'\xFF\xD8':
         # it's a JPEG file
-        #try:
+        # try:
         _find_jpeg_exif(file)
-        #except Exception as e:
-            #print("Except", e)
-
+        # except Exception as e:
+        # print("Except", e)
