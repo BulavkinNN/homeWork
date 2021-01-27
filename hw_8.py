6a052da7eb83de13610aa959572dc4f7b84b49d7
Bulavkin Nikolay <bulnikn@gmail.com>
2021-01-27 19:13:54 +0200

    Merge branch 'master' of https://github.com/BulavkinNN/homeWork
    # Conflicts:
    #	.idea/homeWork.iml
    #	.idea/misc.xml

--- hw_8.py
+++ hw_8.py
@@ -2,6 +2,7 @@ import time
 import logging
 
 # import exifread
+# logging.basicConfig(level=logging.INFO, filename="root.log", filemode="r")
 logging.basicConfig(level=logging.INFO)
 
 
@@ -10,7 +11,7 @@ def timer(func):
         start_timer = time.perf_counter_ns()
         func(*arg, **kwargs)
         stop_timer = time.perf_counter_ns()
-        #logging.debug(f'function {func.__name__}  work {stop_timer - start_timer} ns')
+        # logging.debug(f'function {func.__name__}  work {stop_timer - start_timer} ns')
 
         return stop_timer - start_timer  # ns
 
@@ -22,61 +23,52 @@ def read_from_file(file):
     file.read()
 
 
-buffer_list = [0, 2]
-result_table = dict()
+def test_buffering(file_name="e:\Mult.avi"):
+    buffer_list = [0, 2]
+    result_table = dict()
 
-logging.info('Start reading big file')
-while True:
-    buffer = buffer_list.pop(0)
+    logging.info('Start reading big file')
+    while True:
+        buffer = buffer_list.pop(0)
 
-    try:
-        with open("e:\Mult.avi", mode='rb', buffering=buffer) as file:
-            check_time = read_from_file(file)  #
-    except (OverflowError, MemoryError) as e:
-        logging.error(f'Error when read file {e}')
-        break
-    result_table[buffer] = check_time
-    logging.debug(f"buf= {buffer}, check_time= {check_time:,}")
-    buffer_list.append(buffer_list[-1] * 2)  # Make list [0,2,4,8,16,32 and ....
+        try:
+            with open(file_name, mode='rb', buffering=buffer) as file:
+                check_time = read_from_file(file)  #
+        except (OverflowError, MemoryError) as e:
+            logging.error(f'Error when read file {e}', exc_info=False)
+            break
+        result_table[buffer] = check_time
+        logging.debug(f"buf= {buffer}, check_time= {check_time:,}")
+        buffer_list.append(buffer_list[-1] * 2)  # Make list [0,2,4,8,16,32 and ....
+    return result_table
 
-# print result
-logging.info('Print optimal/non-optimal result')
-for k, v in result_table.items():
-    if v == min(result_table.values()):
-        print(f"Optimal buffering = {k:,}, time = {v:,} ns")
-    if v == max(result_table.values()):
-        print(f"Non-optimal buffering = {k:,}, time = {v:,} ns")
 
-# draw result
-logging.info('Draw result')
-v_max = max(result_table.values())
-scale = (50 / v_max)
-sort_key = sorted(result_table.keys())
-print(f"\nBuffering      *={int(scale)} ns{(' ' * 40):<50} Time, ns\n")
-for key in sort_key:
-    print(f"{key:<13,} {('*' * int(result_table[key] * scale)):<50} {result_table[key]:,}")
+def print_result(result_table):
+    # print result
+    logging.info('Print optimal/non-optimal result')
+    for k, v in result_table.items():
+        if v == min(result_table.values()):
+            print(f"Optimal buffering = {k:,}, time = {v:,} ns")
+        if v == max(result_table.values()):
+            print(f"Non-optimal buffering = {k:,}, time = {v:,} ns")
+    return result_table
+
+
+def draw_result(result_table):
+    # draw result
+    logging.info('Draw result')
+    v_max = max(result_table.values())
+    scale = (v_max / 50)
+    sort_key = sorted(result_table.keys())
+    print(f"{('-' * 75)}")
+    print(f"\nBuffering                 *= {scale: .3} ns{(' ' * 25):<25} Time, ns\n")
+    print(f"{('-' * 75)}")
+    for key in sort_key:
+        print(f"{key:<13,} {('*' * int(result_table[key] / scale)):<50} {result_table[key]:,}")
+    print(f"{('-' * 75)}")
+
 
 # /////////////////////////////////////////
-logging.info("Read EXIF data from jpq file")
-file_name = "hw_8_source/fisherman.jpg"
-file_name = "hw_8_source/1.jpg"
-marker_mapping = {
-    b'\xFF\xd8': "Start of Image",
-    b'\xff\xe0': "Application Default Header",
-    b'\xff\xdb': "Quantization Table",
-    b'\xff\xc0': "Start of Frame",
-    b'\xff\xc4': "Define Huffman Table",
-    b'\xff\xda': "Start of Scan",
-    b'\xff\xd9': "End of Image"
-}
-
-
-def ord_(dta):
-    if isinstance(dta, str):
-        return ord(dta)
-    return dta
-
-
 def _find_jpeg_exif(file):
     file.seek(2)
     marker = file.read(2)
@@ -94,12 +86,38 @@ def _find_jpeg_exif(file):
         logging.debug(f"trumbnail= {int.from_bytes(file.read(1), 'big')}x{int.from_bytes(file.read(1), 'big')}")
 
 
-with open(file_name, "rb") as file:
-    data = file.read(2)
-    logging.debug("data (first 2 bytes)", data)
-    if data[0:2] == b'\xFF\xD8':
-        logging.info("It`s jpeg file")
-        try:
-            _find_jpeg_exif(file)
-        except Exception as e:
-            logging.error(f'Error reading exif from jpeg {e}')
+def read_exif(file_name="hw_8_source/1.jpg"):
+    logging.info("Read EXIF data from jpq file")
+    # file_name = "hw_8_source/fisherman.jpg"
+    # file_name = "hw_8_source/1.jpg"
+    marker_mapping = {
+        b'\xFF\xd8': "Start of Image",
+        b'\xff\xe0': "Application Default Header",
+        b'\xff\xdb': "Quantization Table",
+        b'\xff\xc0': "Start of Frame",
+        b'\xff\xc4': "Define Huffman Table",
+        b'\xff\xda': "Start of Scan",
+        b'\xff\xd9': "End of Image"
+    }
+
+    with open(file_name, "rb") as file:
+        data = file.read(2)
+        logging.debug("data (first 2 bytes)", data)
+        if data[0:2] == b'\xFF\xD8':
+            logging.info("It`s jpeg file")
+            try:
+                _find_jpeg_exif(file)
+            except Exception as e:
+                logging.error(f'Error reading exif from jpeg {e}', exc_info=True)
+
+
+# /////////////////////////////////////////
+def test_logging():
+    logging.warning("Try  logging.exception() not in try: except")
+    logging.exception("Try  logging.exception() not in try: except")
+
+
+# /////////////////////////////////////////
+draw_result(print_result(test_buffering("e:\Mult.avi")))
+read_exif(file_name="hw_8_source/1.jpg")
+test_logging()
